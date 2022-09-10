102025ce699e9dafe6ade507d7621e4a87ac827c
Satyam Verma <satyamverma46@outlook.com>
2022-09-10 10:25:52 +0530

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,7 @@
+def on_button_pressed_a():
+    basic.show_icon(IconNames.HAPPY)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    basic.show_icon(IconNames.SAD)
+input.on_button_pressed(Button.B, on_button_pressed_b)
---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+})
